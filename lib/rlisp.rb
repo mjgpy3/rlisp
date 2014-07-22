208922d4f6798a9d5b6acfa44d13c91afffe29db
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-22 19:42:11 -0400

    Added simple "Hello World" case

--- lib/rlisp.rb
+++ lib/rlisp.rb
@@ -1,2 +1,7 @@
 def Rlisp
+  to_execute = yield
+
+  if to_execute.is_a?(Array)
+    puts to_execute[1] if to_execute.first == :print
+  end
 end
--- spec/lib/rlisp_spec.rb
+++ spec/lib/rlisp_spec.rb
@@ -6,11 +6,20 @@ describe '#Rlisp' do
     subject { Rlisp {} }
 
     specify { expect { subject }.to_not raise_error }
+
+    it { is_expected.to be_nil }
   end
 
   context 'when executing a print list' do
     subject { Rlisp { [:print, thing_to_print] } }
+    let(:thing_to_print) { 'foobar' }
 
     specify { expect { subject }.to_not raise_error }
+
+    context 'with a ruby string' do
+      let(:thing_to_print) { "Hello world\n" }
+
+      specify { expect { subject }.to output(thing_to_print).to_stdout }
+    end
   end
 end
