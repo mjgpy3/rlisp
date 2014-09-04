cb01cba411c0461182ca5e3da4c8ef419c21edba
Michael Gilliland <mjg.py3@gmail.com>
2014-09-03 22:03:11 -0400

    Simplified code slightly

--- lib/rlisp.rb
+++ lib/rlisp.rb
@@ -81,8 +81,7 @@ class RlispExecutor
   def execute_and_perform_lookups(array, lookups)
     after_evaluating_level(array, lookups) do |op, all|
       method = @available_methods[op]
-      return execute(method.(all)) if method.is_a?(Proc)
-      return execute(method.(all), method.lookups) if method.is_a?(CustomMethod)
+      return execute(method.(all), (method.lookups rescue lookups)) unless method.nil?
       return execute(all.first, lookups) if all.size == 1
 
       all.first.is_a?(Symbol) ? all[1].send(all.first, *all[2..-1]) : all
